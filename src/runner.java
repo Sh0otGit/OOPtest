fa3701daf626c69618c9a09481594fe193dc50df
Matthew Davis <machdavis2003@gmail.com>
2023-09-05 20:45:30 -0600

    first commit

--- src/runner.java
+++ src/runner.java
@@ -2,5 +2,6 @@ public class runner {
     public static void main(String[] args){
         Student student = new Student("Matthew", 20, 80734525);
         System.out.println(student.getName()+" is "+student.getAge()+" years old. Id:"+student.getStudentID());
+        //test
     }
 }
